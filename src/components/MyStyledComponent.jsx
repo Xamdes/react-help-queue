76c74914b2f3d5763607f2ea60bc9d54891e8c4a
Steven Colburn <stevencolburn@xamdes.com>
2018-09-04 14:08:25 -0700

    finished monday help queue tutorial

--- src/components/MyStyledComponent.jsx
+++ src/components/MyStyledComponent.jsx
@@ -1,23 +1,28 @@
-#!/usr/bin/env bash
+import React from "react";
+import PropTypes from "prop-types";
 
-cd ./src/components
-
-echo "import React from \"react\";
-import PropTypes from \"prop-types\";
-
-function $1(){
+function MyStyledComponent(){
+  var myStyledComponentStyles = {
+    backgroundColor: '#ecf0f1',
+    fontFamily: 'sans-serif',
+    paddingTop: '50px'
+  }
   return (
-  <div>
-  Insert Code Here
+  <div style={myStyledComponentStyles}>
+    <h1>Hey, I'm a component</h1>
+    <h2>But there's something different about me...</h2>
+    <h3>Unlike other components you have worked with thus far....</h3>
+    <h4>I also include custom CSS styles!</h4>
+    <p>Pretty cool, right</p>
   </div>
   );
 }
 
-$1.propTypes = {
+MyStyledComponent.propTypes = {
   // example: PropTypes.string,
 };
 
-export default $1;
+export default MyStyledComponent;
 
 /*
 propTypes Examples
@@ -34,4 +39,3 @@ exampleArrayOfStrings: PropTypes.arrayOf(PropTypes.string),
 exampleClassTypeProp: PropTypes.instanceOf(ExampleClassName),
 optionalEnum: PropTypes.oneOf(['ExampleClass', 'AnotherExampleClass']),
 */
-" > "$1.jsx"
