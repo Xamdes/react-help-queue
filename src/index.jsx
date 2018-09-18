1edba86ae07012fe1d9b0ac9684f2409002b8b63
Guest User <Guest@epicodus-25.local>
2018-09-18 11:04:16 -0700

    jest setup complete

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import ticketListReducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(ticketListReducer);
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
       <HashRouter>
-        <Component/>
+        <Provider>
+          <Component/>
+        </Provider>
       </HashRouter>
-    </AppContainer>,
+    // </AppContainer>,
     document.getElementById('react-app-root')
   );
 };
